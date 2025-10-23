7071cff4f1b7a43e522a42a96dc0ebb90eef6f6f
w-tsuru <w.turu@largonet.co.jp>
2025-10-23 11:14:59 +0900

    add:doc/RP2_10_結合テスト仕様書_1.1.xls
    テスト仕様書の格納
    
    fix:Case01.java,Case02.java
    
    テストクラスの作成
    
    add:testcase01.png,testcase02_01.png,testcase02_02.png
    
    エビデンスの格納

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,7 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,11 +13,14 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
  * ケース01
  * @author holy
+ * test
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース01 ログイン画面への遷移")
@@ -34,8 +41,17 @@ public class Case01 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
+	void test01() throws Exception {
 		// TODO ここに追加
+		//URLでアクセス
+		goTo("http://localhost:8080/lms/");
+		//タイトル名を取得し、「ログイン｜LMS」であるかを確認
+		String text1 = webDriver.getTitle();
+		assertEquals("ログイン | LMS", text1);
+		System.out.println(text1);
+		//エビデンス取得
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("evidence\\testcase01.png"));
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,7 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +13,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +43,43 @@ public class Case02 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
+	void test01() throws Exception {
 		// TODO ここに追加
+		//URLにアクセス
+		goTo("http://localhost:8080/lms/");
+		//タイトル名を取得し確認
+		String text1 = webDriver.getTitle();
+		assertEquals("ログイン | LMS", text1);
+		System.out.println(text1);
+		//エビデンスの取得
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("evidence\\testcase02_01.png"));
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
+	void test02() throws Exception {
 		// TODO ここに追加
+		//登録されていないIDの入力
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("Test1234");
+
+		//登録されていないパスワードの入力
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys("Test1234", Keys.ENTER);
+
+		//エラーメッセージを取得し確認
+		WebElement errorMessage = webDriver.findElement(By.className("help-inline"));
+		assertEquals("* ログインに失敗しました。", errorMessage.getText());
+
+		//エビデンスの取得
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("evidence\\testcase02_02.png"));
+
 	}
 
 }
